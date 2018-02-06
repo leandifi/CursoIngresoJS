0a77fffb27596a17f171774402d7238fb556e3a2
Di Filippo Leandro <leeandifi@gmail.com>
2018-02-06 10:15:31 -0300

    Ejercicio 1 y 2 ( primera clase )
    variables, alert, prompt, comentarios.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,23 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre;
+
+	nombre=prompt("ingrese nombre", "natalia natalia");
+
+	alert("su nombre es: "+nombre);
+
+	/*
+	esto es un bloque
+	alert(nombre);
+	alert("nombre");
+	nombre="Leandro";
+	alert(nombre);*/
+
+	//var person = prompt("Please enter your name", "Harry Potter");
+
+
+
+
 }
 
