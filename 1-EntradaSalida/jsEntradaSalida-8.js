c3e8629bc78e1dbcef10a4414832a371b486cf9f
Di Filippo Leandro <leeandifi@gmail.com>
2018-02-07 11:46:10 -0300

    Ej 6,7,8,9,10.

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,20 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var numeroDividendo;
+	var numeroDivisor;
+	var resto;
+
+	numeroDividendo=document.getElementById('numeroDividendo').value;
+	numeroDivisor=document.getElementById('numeroDivisor').value;
+
+	numeroDividendo=parseInt(numeroDividendo);
+	numeroDivisor=parseInt(numeroDivisor);
+
+	resto=numeroDividendo%numeroDivisor;
+
+	alert("El resto es: "+resto);
+
+
+				
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,23 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var sueldo;
+	var aumento;
+	var resultado;
+
+
+	sueldo=document.getElementById('sueldo').value;
+	
+	sueldo=parseInt(sueldo);
+
+	aumento=(sueldo*10)/100;
+	resultado=sueldo+aumento;
+
+	document.getElementById('resultado').value=resultado;
+
+	
+
+	
 
 	
 }
