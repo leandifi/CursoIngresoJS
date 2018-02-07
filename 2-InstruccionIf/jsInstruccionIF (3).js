6001a2206c31e6785c2b50fc20b0354646fcb391
Di Filippo Leandro <leeandifi@gmail.com>
2018-02-07 11:46:40 -0300

    IF

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -2,5 +2,18 @@ function Mostrar()
 {
 //tomo la edad  
 
+	var edad;
+
+	edad=document.getElementById('edad').value;
+
+	if(edad>17)
+	{
+		alert("Es mayor de edad.");
+	}
+	if(edad<=17)
+	{
+		alert("Es menor de edad.");
+	}
+
 
 }//FIN DE LA FUNCIÓN
