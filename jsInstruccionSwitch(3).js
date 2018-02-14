dbdf00eb506acf5016fa554d9fcd020760ed06c4
Di Filippo Leandro <leeandifi@gmail.com>
2018-02-14 11:43:46 -0300

    default
    default

---
+++ jsInstruccionSwitch(3).js
@@ -0,0 +1,20 @@
+function Mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+	switch(mesDelAño)
+	{
+		case "Febrero":
+			alert("Este mes no tiene más de 29 dias.");
+			break;
+		default :
+		alert("Este mes tiene 30 o más días");
+	}
+
+//alert (mesDelAño);
+	
+	
+
+
+}//FIN DE LA FUNCIÓN
