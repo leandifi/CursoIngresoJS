ec2d84ef9cfd9c46e445feac099bbd1320f4751b
Di Filippo Leandro <leeandifi@gmail.com>
2018-02-14 09:57:20 -0300

    Ejercicio 8 corregido y 10,
    random.

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,31 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+
+	var numeroAleatorio;
+
+	numeroAleatorio=Math.floor(Math.random()*10+1);
+	//alert(numeroAleatorio);
+	
+
+	if( numeroAleatorio == 9 || numeroAleatorio == 10){
+
+		alert(numeroAleatorio+" EXCELENTE");
+	}else{
+
+
+		if(numeroAleatorio>=4 && numeroAleatorio<9){
+
+		alert(numeroAleatorio+" APROBÓ");
+
+		}else{
+			if(numeroAleatorio<4){
+
+			alert(numeroAleatorio+" Vamos la próxima se puede");
+			}
+		}
+	}
+
 	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -4,16 +4,24 @@ function Mostrar()
 	
 	var edad;
 	var estadoCivil;
-
+	
 	edad=document.getElementById('edad').value;
 	estadoCivil=document.getElementById('estadoCivil').value;	
+
 
 	if(edad>=18 && estadoCivil=="Soltero")
 	{
 
 		alert("Es soltero y no es menor.");
 
-	}
+	}else
+		{
+			if (edad<18 && estadoCivil!="Soltero") {
+
+			
+			console.log("NO HACER NADA.");
+			}
+		}
 	
 
 
