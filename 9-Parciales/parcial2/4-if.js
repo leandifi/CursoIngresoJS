f52c64c186118cf0800a23610485bfed5d4bbbc3
Di Filippo Leandro <leeandifi@gmail.com>
2018-02-28 11:37:51 -0300

    Clase 28/02

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,32 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+/*No se tiene que validar ni usar while;Se ingresan dos numeros: si son iguales
+se concatenan. si el mayor es el primero se multiplican, de lo contrario se restan. Si la multiplicacion es par: mostrar "es par"*/
+
 function Mostrar()
 {
-	alert("Funciona 4-if");
+	//alert("Funciona 4-if");
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno=prompt("Ingrese un numero:");
+	numeroDos=prompt("Ingrese el segundo numero.");
+	if(numeroUno==numeroDos){
+		resultado=numeroUno+numeroDos
+	}else{
+		numeroUno=parseInt(numeroUno);
+		numeroDos=parseInt(numeroDos);
+		if(numeroUno>numeroDos){
+			resultado=numeroUno*numeroDos
+		}else{
+			resultado=numeroUno-numeroDos;
+		}
+	}
+	if((numeroUno*numeroDos)%2==0 && (numeroUno*numeroDos)!=0){
+		alert(resultado+" es par.");
+	}else{
+		alert(resultado);
+	}
 	
 }
 
--- 9-Parciales/parcial2/8-iteraciones.js
+++ 9-Parciales/parcial2/8-iteraciones.js
@@ -1,7 +1,72 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+/*Se ingresa hasta que el usuario quiera: El nombre de un animal. El peso del mismo(validar que sea mayor a 0) y la temperatura
+del habitat de este animal (validad entre -40 y 40). Informar la cantidad de temperaturas pares, la cantidad de temperaturas
+impares, el nombre del animal mas pesasdo, el nombre del animal menos pesado, la cantidad de animales que viven en habitat moneres
+a 0 inclusive. El promedio del peso de todos los animales. La temperatura maxima y la minima*/
 function Mostrar()
 {
-	alert("Funciona 8-iteraciones");
-	
+	//alert("Funciona 8-iteraciones");
+	var nombre;
+	var peso;
+	var temperatura;
+	var continuar="no";
+	var temperaturaPar=0;
+	var temperaturaImpar=0;
+	var pesoMaximo=-999;
+	var pesoMinimo=999;
+	var nombreMaximo;
+	var nombreMinimo;
+
+	while(continuar!="no")
+	{
+		nombre=prompt("Ingrese el nombre de un animal:");
+		peso=prompt("Ingrese el peso del mismo:");
+		peso=parseInt(peso);
+		while(peso<0)
+		{
+			peso=prompt("Ingrese un peso valido (mayor a 0):");
+			peso=parseInt(peso);
+		}
+		temperatura=prompt("Ingrese la temperatura de su habitat:");
+		temperatura=parseInt(temperatura);
+		while(temperatura<-40 || temperatura>40)
+		{
+			temperatura=prompt("Ingrese una temperatura valida:");
+			temperatura=parseInt(temperatura);
+		}
+		if(temperatura%2==0 && temperatura!=0)
+		{
+			temperaturaPar++;
+		}else
+		{
+			if(!=0)
+			{
+				temperaturaImpar++;
+			}
+		}
+		if(contador==1)
+		{
+			pesoMaximo=peso;
+			pesiMinimo=peso
+			nombreMaximo=nombre;
+			nombreMinimo=nombre;
+		}else
+		{
+			if(peso>pesoMaximo)
+			{
+				pesoMaximo=peso;
+				nombreMaximo=nombre;
+			}else
+			{
+				if(peso<pesoMinimo)
+				{
+					pesoMinimo=peso;
+					nombreMinimo=nombre;
+				}
+			}
+		}
+
+	}
+	alert("El que mas pesa es: "+nombreMaximo)
 }
 
